2f57f79862062b4ccca727d1a4a20c108d6434d0
Chen Yang <yangc5@mail.uc.edu>
2016-08-10 20:04:00 -0500

    initial commit

---
+++ js/application.js
@@ -0,0 +1,11 @@
+// Here's the initial data. Again: don't worry about persistence :)
+data = [
+  { name: "Mark-Paul Gosselaar", photo_url: "" },
+  { name: "Delta Burke", photo_url: "img/avatars/delta.png" },
+  { name: "Alf", photo_url: "img/avatars/alf.png" },
+  { name: "Jaleel White", photo_url: "img/avatars/jaleel.png" },
+  { name: "Ralph Macchio", photo_url: "img/avatars/ralph.png" },
+  { name: "Candace Cameron", photo_url: "img/avatars/candace.png" },
+  { name: "Patrick Duffy", photo_url: "img/avatars/pduff.png" },
+  { name: "Arnold Schwartzengger", photo_url: "img/avatars/arnold.png" }
+];
