6c1cfa0afe6e3d5f89393fcca3dccf7d7fd88f2b
Chen Yang <yangc5@mail.uc.edu>
2016-08-12 08:00:35 -0500

    cleanup

--- js/application.js
+++ js/application.js
@@ -12,15 +12,28 @@ var data = [
   { name: "Arnold Schwartzengger", photo_url: "img/avatars/arnold.png" }
 ];
 
+function makeCard(name, imageUrl){
+    var card = '<div class="card four columns"> <div class="five columns"> <div class="image-wrapper"> <img src="'+imageUrl+'" onerror="this.src=\'/img/default.png\'" alt=""> </div> </div> <div class="seven columns"> <div class="name-wrapper"> '+name+' </div> </div> <img class="close" src="img/close.png" alt=""> </div>';
+    return card;
+}
+
 $(document).ready(function(){
+  //populate data
+  var name, imageUrl, card;
+
+  data.forEach(function(celeb){
+    name = celeb.name;
+    imageUrl = celeb.photo_url;
+    card = makeCard(name, imageUrl);
+    $('.cards').prepend(card);
+  });
+
   //form submission
   $('form').submit(function(event){
     event.preventDefault();
-    var name = $('input[name="name"]').val();
-    var imageUrl = $('input[name="photo_url"]').val();
-
-    var card = '<div class="card four columns"> <div class="five columns"> <div class="image-wrapper"> <img src="'+imageUrl+'" onerror="this.src=\'/img/default.png\'" alt=""> </div> </div> <div class="seven columns"> <div class="name-wrapper"> '+name+' </div> </div> <img class="close" src="img/close.png" alt=""> </div>';
-
+    name = $('input[name="name"]').val();
+    imageUrl = $('input[name="photo_url"]').val();
+    card = makeCard(name, imageUrl);
     $('.cards').prepend(card);
   });
 
@@ -36,5 +49,4 @@ $(document).ready(function(){
   $('.cards').on('click', '.close', function(){
     $(this).parent().remove();
   })
-
 });
