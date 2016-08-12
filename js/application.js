93d0b8cc9d61eeadc759340e6b5579877d3653f0
Chen Yang <yangc5@mail.uc.edu>
2016-08-12 07:49:59 -0500

    removal cards working

--- js/application.js
+++ js/application.js
@@ -13,6 +13,7 @@ var data = [
 ];
 
 $(document).ready(function(){
+  //form submission
   $('form').submit(function(event){
     event.preventDefault();
     var name = $('input[name="name"]').val();
@@ -20,18 +21,20 @@ $(document).ready(function(){
 
     var card = '<div class="card four columns"> <div class="five columns"> <div class="image-wrapper"> <img src="'+imageUrl+'" onerror="this.src=\'/img/default.png\'" alt=""> </div> </div> <div class="seven columns"> <div class="name-wrapper"> '+name+' </div> </div> <img class="close" src="img/close.png" alt=""> </div>';
 
-    $('.cards').append(card);
+    $('.cards').prepend(card);
   });
 
+  //hovering
   $('.cards').on('mouseenter', '.card', function(){
       $(this).find('.close').addClass('hover');
   });
-
   $('.cards').on('mouseleave', '.card', function(){
       $(this).find('.close').removeClass('hover');
   });
 
-
-
+  //remove cards
+  $('.cards').on('click', '.close', function(){
+    $(this).parent().remove();
+  })
 
 });
