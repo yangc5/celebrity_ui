2ea2e66a175ccb33b3c61e5248bc1d01030db90a
Chen Yang <yangc5@mail.uc.edu>
2016-08-11 20:52:38 -0500

    able to add employees

--- js/application.js
+++ js/application.js
@@ -1,5 +1,7 @@
 // Here's the initial data. Again: don't worry about persistence :)
-data = [
+'use strict';
+
+var data = [
   { name: "Mark-Paul Gosselaar", photo_url: "" },
   { name: "Delta Burke", photo_url: "img/avatars/delta.png" },
   { name: "Alf", photo_url: "img/avatars/alf.png" },
@@ -9,3 +11,15 @@ data = [
   { name: "Patrick Duffy", photo_url: "img/avatars/pduff.png" },
   { name: "Arnold Schwartzengger", photo_url: "img/avatars/arnold.png" }
 ];
+
+$(document).ready(function(){
+  $('form').submit(function(event){
+    event.preventDefault();
+    var name = $('input[name="name"]').val();
+    var imageUrl = $('input[name="photo_url"]').val();
+
+    var card = '<div class="card four columns"> <div class="five columns"> <div class="image-wrapper"> <img src="'+imageUrl+'" onerror="this.src=\'/img/default.png\'" alt=""> </div> </div> <div class="seven columns"> <div class="name-wrapper"> '+name+' </div> </div> <img class="close" src="img/close.png" alt=""> </div>';
+
+    $('.cards').append(card);
+  })
+});
